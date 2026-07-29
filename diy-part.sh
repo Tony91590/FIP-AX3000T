c4b6a5d211b25c6e0fcc8fb88322eba2e3f197b5
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2026-07-29 20:30:51 +0200

    Change patch file reference to mesh-button.patch

--- diy-part.sh
+++ diy-part.sh
@@ -1,5 +1,5 @@
 # diy ATF and u-boot for redmi ax6000 based on mt798x
 
-PATCH_FILE="$GITHUB_WORKSPACE/fix-layout.patch"
+PATCH_FILE="$GITHUB_WORKSPACE/mesh-button.patch"
 patch -p1 < "$PATCH_FILE"
 
