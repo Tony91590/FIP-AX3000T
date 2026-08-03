5840c2c282008e2c04a32859ae37e1c9c0db7232
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2026-08-03 17:12:46 +0200

    Remove old pll.c.orig file for new version

--- diy-part.sh
+++ diy-part.sh
@@ -4,4 +4,5 @@ PATCH_FILE="$GITHUB_WORKSPACE/diff.patch"
 patch -p1 < "$PATCH_FILE"
 
 rm -f atf-20240117-bacca82a8/plat/mediatek/mt7981/drivers/pll/pll.c.orig
+rm -f atf-20250711/plat/mediatek/mt7981/drivers/pll/pll.c.orig
 rm -f uboot-mtk-20250711/arch/arm/dts/mt7981-ax3000t.dts.orig
