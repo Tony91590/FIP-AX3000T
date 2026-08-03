f6e5abeb4ec5f3f65bae7d809311aa099019c15d
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2026-08-03 15:42:30 +0200

    Modify script for Xiaomi AX3000T support
    Updated script for Xiaomi AX3000T and added patching commands.

--- diy-part.sh
+++ diy-part.sh
@@ -1,2 +1,8 @@
-# diy ATF and u-boot for redmi ax6000 based on mt798x
+# diy ATF and u-boot for xiaomi ax3000t based on mt798x
 
+PATCH_FILE="$GITHUB_WORKSPACE/diff.patch"
+patch -p1 < "$PATCH_FILE"
+
+rm -f uboot-mtk-20230718-09eda825/cmd/glbtn.c.orig
+rm -f atf-20240117-bacca82a8/plat/mediatek/mt7981/drivers/pll/pll.c.orig
+rm -f uboot-mtk-20230718-09eda825/arch/arm/dts/mt7981-ax3000t.dts.orig
