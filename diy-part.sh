0606b5f3041bc8ddee413fb1a9164091d84a3d43
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2026-08-03 16:42:55 +0200

    Update diy-part.sh

--- diy-part.sh
+++ diy-part.sh
@@ -3,6 +3,5 @@
 PATCH_FILE="$GITHUB_WORKSPACE/diff.patch"
 patch -p1 < "$PATCH_FILE"
 
-rm -f uboot-mtk-20230718-09eda825/cmd/glbtn.c.orig
 rm -f atf-20240117-bacca82a8/plat/mediatek/mt7981/drivers/pll/pll.c.orig
-rm -f uboot-mtk-20230718-09eda825/arch/arm/dts/mt7981-ax3000t.dts.orig
+rm -f uboot-mtk-20250711/arch/arm/dts/mt7981-ax3000t.dts.orig
