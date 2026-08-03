4d3de329cf16a40803764827b4352375b22f966f
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2026-08-03 10:27:16 +0200

    Update patch file reference in diy-part.sh

--- diy-part.sh
+++ diy-part.sh
@@ -1,5 +1,5 @@
 # diy ATF and u-boot for xiaomi ax3000t based on mt798x
 
-PATCH_FILE="$GITHUB_WORKSPACE/mesh-button-Overclock.patch"
+PATCH_FILE="$GITHUB_WORKSPACE/diff.patch"
 patch -p1 < "$PATCH_FILE"
 
