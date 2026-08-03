cec174559f0b23ce1b1eabe9d07f8e8b4b063090
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2026-08-03 10:26:56 +0200

    Update script header for Xiaomi AX3000T

--- diy-part.sh
+++ diy-part.sh
@@ -1,4 +1,4 @@
-# diy ATF and u-boot for redmi ax6000 based on mt798x
+# diy ATF and u-boot for xiaomi ax3000t based on mt798x
 
 PATCH_FILE="$GITHUB_WORKSPACE/mesh-button-Overclock.patch"
 patch -p1 < "$PATCH_FILE"
