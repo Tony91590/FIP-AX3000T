4367e77a980511b089bee2b8c2d28253208ae464
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2026-08-04 22:35:30 +0200

    Update file paths for removing original files

--- diy-part.sh
+++ diy-part.sh
@@ -3,5 +3,5 @@
 PATCH_FILE="$GITHUB_WORKSPACE/diff.patch"
 patch -p1 < "$PATCH_FILE"
 
-rm -f uboot-mtk-20230718-09eda825/cmd/glbtn.c.orig
-rm -f uboot-mtk-20230718-09eda825/arch/arm/dts/mt7981-ax3000t.dts.orig
+rm -f uboot-mtk-20220606/cmd/glbtn.c.orig
+rm -f uboot-mtk-20220606/arch/arm/dts/mt7981-ax3000t.dts.orig
