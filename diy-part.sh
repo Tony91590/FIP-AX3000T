53e531d16a2070fa3d4551276b6cdcf0b8a983c4
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2026-08-04 19:10:59 +0200

    Remove original files for pll.h and update cleanup

--- diy-part.sh
+++ diy-part.sh
@@ -5,4 +5,5 @@ patch -p1 < "$PATCH_FILE"
 
 rm -f uboot-mtk-20230718-09eda825/cmd/glbtn.c.orig
 rm -f atf-20240117-bacca82a8/plat/mediatek/mt7981/drivers/pll/pll.c.orig
+rm -f atf-20240117-bacca82a8/plat/mediatek/mt7981/drivers/pll/pll.h.orig
 rm -f uboot-mtk-20230718-09eda825/arch/arm/dts/mt7981-ax3000t.dts.orig
