1943c303e7f38b2ecac26334219bb487489a3c7e
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2026-08-05 17:28:00 +0200

    Remove additional orig files from cleanup script

--- diy-part.sh
+++ diy-part.sh
@@ -29,3 +29,5 @@ rm -f atf-20220606-637ba581b/plat/mediatek/mt7981/drivers/pll/pll.c.orig
 rm -f atf-20240117-bacca82a8/plat/mediatek/mt7981/drivers/pll/pll.c.orig
 rm -f atf-20240117-bacca82a8/plat/mediatek/mt7981/drivers/pll/pll.h.orig
 rm -f uboot-mtk-20230718-09eda825/arch/arm/dts/mt7981-ax3000t.dts.orig
+rm -f uboot-mtk-20220606/cmd/glbtn.c.orig
+rm -f uboot-mtk-20220606/arch/arm/dts/mt7981-ax3000t.dts.orig
