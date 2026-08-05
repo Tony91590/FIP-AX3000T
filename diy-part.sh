786870827c362817776579a67e35708f0514411b
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2026-08-05 11:30:27 +0200

    Implement version selection for U-Boot and ATF
    Added version selection for U-Boot and ATF in the script.

--- diy-part.sh
+++ diy-part.sh
@@ -1,18 +1,25 @@
 # diy ATF and u-boot for xiaomi ax3000t based on mt798x
 
-# UBOOT_DIR=uboot-mtk-20220606
-# UBOOT_DIR=uboot-mtk-20230718-09eda825
-# ATF_DIR=atf-20220606-637ba581b
-# ATF_DIR=atf-20231013-0ea67d76a
-# ATF_DIR=atf-20240117-bacca82a8
-
 BUILD_SH="build.sh"
 
-# Modifier U-Boot
-sed -i 's/^UBOOT_DIR=.*/UBOOT_DIR=uboot-mtk-20230718-09eda825/' "$BUILD_SH"
+# Choix des versions
+UBOOT_SEL=2
+ATF_SEL=2
 
-# Modifier ATF
-sed -i 's/^ATF_DIR=.*/ATF_DIR=atf-20231013-0ea67d76a/' "$BUILD_SH"
+
+case $UBOOT_SEL in
+    1) UBOOT="uboot-mtk-20220606" ;;
+    2) UBOOT="uboot-mtk-20230718-09eda825" ;;
+esac
+
+case $ATF_SEL in
+    1) ATF="atf-20220606-637ba581b" ;;
+    2) ATF="atf-20231013-0ea67d76a" ;;
+    3) ATF="atf-20240117-bacca82a8" ;;
+esac
+
+sed -i "s|^# *UBOOT_DIR=.*|UBOOT_DIR=$UBOOT|" "$BUILD_SH"
+sed -i "s|^# *ATF_DIR=.*|ATF_DIR=$ATF|" "$BUILD_SH"
 
 git clone -b 2023 https://github.com/Tony91590/bl-mt798x.git tmp_imm
 cp -r tmp_imm/atf-20231013-0ea67d76a ./
