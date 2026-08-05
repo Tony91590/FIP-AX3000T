b1a63b3d50e905fb68b172ee5ccec9994a49cd4f
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2026-08-05 17:24:18 +0200

    Update FIP boot configuration in diy-part.sh

--- diy-part.sh
+++ diy-part.sh
@@ -1,5 +1,8 @@
 # diy ATF and u-boot for xiaomi ax3000t based on mt798x
 
+sed -i 's/^CONFIG_TARGET_FIP_NO_SEC_BOOT=.*/CONFIG_TARGET_ALL_NO_SEC_BOOT=y/' \
+    atf-20220606-637ba581b/configs/mt7981_ax3000t_defconfig
+    
 PATCH_FILE="$GITHUB_WORKSPACE/diff.patch"
 patch -p1 < "$PATCH_FILE"
 
