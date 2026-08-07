a3ddb0f5cfa2cc835a032bfde9100922b440e0c2
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2026-08-08 01:12:16 +0200

    Update comment to include patch file reference

--- diy-part.sh
+++ diy-part.sh
@@ -1,4 +1,4 @@
-# diy ATF and u-boot for xiaomi ax3000t based on mt798x
+# diy ATF and u-boot for xiaomi ax3000t based on mt798x [arm-trusted-firmware-mediatek/patches/0999-mt7921-oc-1.6G.patch]
 
 PATCH_FILE="$GITHUB_WORKSPACE/diff.patch"
 patch -p1 < "$PATCH_FILE"
