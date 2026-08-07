c14af45640d9866d0c1b65dd9df83f705cd6a25d
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2026-08-07 13:37:10 +0200

    Update UBOOT and ATF selection values

--- diy-part.sh
+++ diy-part.sh
@@ -2,8 +2,8 @@
 
 BUILD_SH="build.sh"
 
-UBOOT_SEL=2
-ATF_SEL=3
+UBOOT_SEL=1
+ATF_SEL=1
 
 case $UBOOT_SEL in
     1) UBOOT="uboot-mtk-20220606" ;;
@@ -46,7 +46,7 @@ rm -f uboot-mtk-20250711/arch/arm/dts/mt7981-ax3000t.dts.orig
 # 4 = 1640MHz 0x52000000
 # 5 = 1700MHz 0x55000000
 
-FREQ_SELECT=2
+FREQ_SELECT=3
 
 
 case "$FREQ_SELECT" in
