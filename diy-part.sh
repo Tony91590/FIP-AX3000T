7235a4bd8944b5c3986dba264fb4ffc2a3c000b0
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2026-08-09 02:53:45 +0200

    Update file removal commands for new ATF version

--- diy-part.sh
+++ diy-part.sh
@@ -5,5 +5,5 @@ patch -p1 < "$PATCH_FILE"
 
 rm -f uboot-mtk-20230718-09eda825/cmd/glbtn.c.orig
 rm -f uboot-mtk-20230718-09eda825/arch/arm/dts/mt7981-ax3000t.dts.orig
-rm -f atf-20250711/plat/mediatek/mt7981/drivers/pll/pll.c.orig
-rm -f atf-20250711/plat/mediatek/mt7981/drivers/pll/pll.h.orig
+rm -f atf-20260123/plat/mediatek/mt7981/drivers/pll/pll.c.orig
+rm -f atf-20260123/plat/mediatek/mt7981/drivers/pll/pll.h.orig
