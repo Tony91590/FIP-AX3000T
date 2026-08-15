5ee01623edc3ed8c7d1f145d526c4a589877de5f
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2026-08-15 19:24:09 +0200

    Clean up original files from U-Boot and ATF
    Remove original files for specific versions of U-Boot and ATF.

--- diy-part.sh
+++ diy-part.sh
@@ -21,6 +21,11 @@ sed -i -E "s|^[# ]*ATF_DIR=.*|ATF_DIR=$ATF|" "$BUILD_SH"
 PATCH_FILE="$GITHUB_WORKSPACE/diff.patch"
 patch -p1 < "$PATCH_FILE"
 
+rm -f uboot-mtk-20220606/cmd/glbtn.c.orig
+rm -f uboot-mtk-20220606/arch/arm/dts/mt7981-ax3000t.dts.orig
 rm -f uboot-mtk-20230718-09eda825/cmd/glbtn.c.orig
 rm -f uboot-mtk-20230718-09eda825/arch/arm/dts/mt7981-ax3000t.dts.orig
 rm -f atf-20240117-bacca82a8/plat/mediatek/mt7981/drivers/pll/pll.c.orig
+rm -f atf-20220606-637ba581b/plat/mediatek/mt7981/drivers/pll/pll.c.orig
+rm -f atf-20220606-637ba581b/configs/mt7981_ax3000t_defconfig.orig
+
