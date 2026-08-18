46fe76df2e0e5d48d8e4c991f006aaa18c5dffba
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2026-08-18 21:21:44 +0200

    Remove original header file for pll driver

--- diy-part.sh
+++ diy-part.sh
@@ -6,3 +6,4 @@ patch -p1 < "$PATCH_FILE"
 rm -f uboot-mtk-20230718-09eda825/cmd/glbtn.c.orig
 rm -f uboot-mtk-20230718-09eda825/arch/arm/dts/mt7981-ax3000t.dts.orig
 rm -f atf-20240117-bacca82a8/plat/mediatek/mt7981/drivers/pll/pll.c.orig
+rm -f atf-20240117-bacca82a8/plat/mediatek/mt7981/drivers/pll/pll.h.orig
