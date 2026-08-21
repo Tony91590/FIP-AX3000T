b8ab7712ea827230e0463a8558663d3e7490da4c
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2026-08-21 17:27:09 +0200

    Clone repository and copy ATF files

--- diy-part.sh
+++ diy-part.sh
@@ -21,6 +21,10 @@ sed -i -E "s|^[# ]*ATF_DIR=.*|ATF_DIR=$ATF|" "$BUILD_SH"
 PATCH_FILE="$GITHUB_WORKSPACE/diff.patch"
 patch -p1 < "$PATCH_FILE"
 
+git clone -b 2023 https://github.com/Tony91590/bl-mt798x.git tmp_imm
+cp -r tmp_imm/atf-20231013-0ea67d76a ./
+rm -rf tmp_imm
+
 rm -f uboot-mtk-20220606/cmd/glbtn.c.orig
 rm -f uboot-mtk-20220606/arch/arm/dts/mt7981-ax3000t.dts.orig
 rm -f atf-20220606-637ba581b/configs/mt7981_ax3000t_defconfig.orig
