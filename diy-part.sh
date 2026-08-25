e36967464d3414cde95501f23b249d34d15e67e1
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2026-08-25 14:41:05 +0200

    Update diy-part.sh

--- diy-part.sh
+++ diy-part.sh
@@ -1,20 +1,8 @@
 # diy ATF and u-boot for xiaomi ax3000t based on mt798x
 
-rm -f build.sh
-
-git clone -b test https://github.com/Tony91590/bl-mt798x.git tmp_imm
-cp -r tmp_imm/build.sh ./
-rm -rf tmp_imm
-
-rm -rf uboot-mtk-20230718-09eda825
-
-git clone -b new https://github.com/Tony91590/bl-mt798x-dhcpd.git tmp_imm
-cp -r tmp_imm/uboot-mtk-20230718-09eda825 ./
-rm -rf tmp_imm
-
 BUILD_SH="build.sh"
 
-UBOOT_SEL=2
+UBOOT_SEL=1
 ATF_SEL=3
 
 case $UBOOT_SEL in
@@ -33,10 +21,6 @@ sed -i -E "s|^[# ]*ATF_DIR=.*|ATF_DIR=$ATF|" "$BUILD_SH"
 
 PATCH_FILE="$GITHUB_WORKSPACE/diff.patch"
 patch -p1 < "$PATCH_FILE"
-
-git clone -b test https://github.com/Tony91590/bl-mt798x.git tmp_imm
-cp -r tmp_imm/atf-20240117-bacca82a8 ./
-rm -rf tmp_imm
 
 rm -f uboot-mtk-20220606/cmd/glbtn.c.orig
 rm -f uboot-mtk-20220606/arch/arm/dts/mt7981-ax3000t.dts.orig
