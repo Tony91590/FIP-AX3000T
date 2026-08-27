d1b776b2cc3c9ccf987226a8feb594bded7d8019
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2026-08-27 16:54:06 +0200

    Replace patch command with git apply

--- diy-part.sh
+++ diy-part.sh
@@ -1,7 +1,3 @@
 # diy ATF and u-boot for xiaomi ax3000t based on mt798x
 
-PATCH_FILE="$GITHUB_WORKSPACE/diff.patch"
-patch -p1 < "$PATCH_FILE"
-
-rm -f uboot-mtk-20230718-09eda825/cmd/glbtn.c.orig
-rm -f uboot-mtk-20230718-09eda825/arch/arm/dts/mt7981-ax3000t.dts.orig
+git apply --verbose "diff.patch"
