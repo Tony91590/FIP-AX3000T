be760cc547ad5071113d5a3faf80328a2c49d0e5
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2026-08-30 11:51:01 +0200

    Change ATF_SEL value and update ATF selection

--- diy-part.sh
+++ diy-part.sh
@@ -3,7 +3,7 @@
 BUILD_SH="build.sh"
 
 UBOOT_SEL=1
-ATF_SEL=3
+ATF_SEL=1
 
 case $UBOOT_SEL in
     1) UBOOT="uboot-mtk-20220606" ;;
@@ -12,8 +12,7 @@ esac
 
 case $ATF_SEL in
     1) ATF="atf-20220606-637ba581b" ;;
-    2) ATF="atf-20231013-0ea67d76a" ;;
-    3) ATF="atf-20240117-bacca82a8" ;;
+    2) ATF="atf-20240117-bacca82a8" ;;
 esac
 
 sed -i -E "s|^[# ]*UBOOT_DIR=.*|UBOOT_DIR=$UBOOT|" "$BUILD_SH"
