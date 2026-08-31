42e62cce3da5d72e8efef8466ce8065a2c18d0dc
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2026-08-31 23:44:09 +0200

    Remove original files from ATF directory

--- diy-part.sh
+++ diy-part.sh
@@ -96,3 +96,5 @@ rm -f uboot-mtk-20230718-09eda825/cmd/glbtn.c.orig
 rm -f uboot-mtk-20230718-09eda825/arch/arm/dts/mt7981-ax3000t.dts.orig
 rm -f uboot-mtk-20220606/cmd/glbtn.c.orig
 rm -f uboot-mtk-20220606/arch/arm/dts/mt7981-ax3000t.dts.orig
+rm -f atf-20240117-bacca82a8/plat/mediatek/mt7981/drivers/pll/pll.c.orig
+rm -f atf-20240117-bacca82a8/plat/mediatek/mt7981/drivers/pll/pll.h.orig
