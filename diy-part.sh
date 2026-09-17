03aae02ef76bfcc2dac271a0d519bc16c0971f4f
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2026-09-17 18:22:39 +0200

    Change git apply to patch command in diy-part.sh

--- diy-part.sh
+++ diy-part.sh
@@ -1,4 +1,4 @@
 # diy ATF and u-boot for xiaomi ax3000t based on mt798x [arm-trusted-firmware-mediatek/patches/0999-mt7921-oc-1.6G.patch]
 
 PATCH_FILE="$GITHUB_WORKSPACE/diff.patch"
-git apply -p1 "$PATCH_FILE"
+patch -p1 < "$PATCH_FILE"
